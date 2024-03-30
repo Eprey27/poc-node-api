548518f4442f354ce6777608d7f8919eeb8e2aae
Emilio <eprey27@gmail.com>
2024-03-30 18:41:54 +0100

    fix swagger port v1.2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swaggerOptions');
 
 const app = express();
-const port = 3002;
+const port = 3000;
 
 app.use(express.json());
 
