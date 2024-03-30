5bac56e2ede99d2fef4d4c858d6f98624537d811
Emilio <eprey27@gmail.com>
2024-03-30 20:18:12 +0100

    clean app.js v1.2.0

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
+// Importaciones de módulos externos
 const express = require('express');
+const dotenv = require('dotenv');
 const swaggerUi = require('swagger-ui-express');
+
+// Importaciones de configuraciones y middlewares locales
 const swaggerDocument = require('./swaggerOptions');
 const loggerMiddleware = require('./middlewares/logger');
+const routes = require('./routes/index'); // Asume un archivo index.js en la carpeta de rutas
 
-const routes = require('./routes/index'); // Supone la existencia de un archivo index.js en la carpeta de rutas
+// Configuración de las variables de entorno
+// Decidir qué archivo .env usar basado en NODE_ENV y cargar las variables de entorno
+const envFile = `.env.${process.env.NODE_ENV}`;
+dotenv.config({ path: envFile });
 
+// Configuración inicial de Express
 const app = express();
-const PORT = process.env.PORT || 3000; // Usar variable de entorno o default
+const PORT = process.env.PORT || 3000; // Usar variable de entorno o valor predeterminado
 const HOST = process.env.HOST || 'localhost';
 
-app.use(express.json());
+// Middlewares
+app.use(express.json()); // Para parsear el cuerpo de las peticiones JSON
 app.use(loggerMiddleware); // Middleware personalizado para logging
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// Rutas
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); // Documentación de la API con Swagger
+app.use(routes); // Usar las rutas definidas en routes/index.js
 
-// Usar las rutas importadas
-app.use(routes);
-
+// Iniciar el servidor
 app.listen(PORT, () => {
-    console.log(`La aplicación está escuchando en http://localhost:${PORT}`);
+    console.log(`La aplicación está escuchando en http://${HOST}:${PORT}`);
 });
